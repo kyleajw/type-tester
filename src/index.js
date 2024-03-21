75f62f354058b5d306e35fd94621c4021ca475d1
Kyle Whelan <kyle.aj.whelan@outlook.com>
2024-03-21 21:43:25 +0000

    added restart button

--- src/index.js
+++ src/index.js
@@ -3,11 +3,16 @@ import { words } from './common.json'
 let currentWord = 0;
 let wordIndex = 0;
 const wordBox = document.getElementById('word-box');
+const restartBtn = document.getElementById('restart');
 
 initalise()
 
+restartBtn.addEventListener("click", e => {
+    initalise();
+})
+
 document.addEventListener("keydown", e => {
-    if (!(e.key == 'Alt' || e.key == 'Control' || e.key == 'Shift')) {
+    if (!(e.key == 'Alt' || e.key == 'Control' || e.key == 'Shift' || e.key == 'Tab' || e.key == 'Enter')) {
         const word = document.querySelectorAll('.word')[currentWord];
         const wordLength = word.querySelectorAll('.letter').length;
         const letter = word.querySelectorAll('.letter')[wordIndex];
@@ -35,22 +40,15 @@ document.addEventListener("keydown", e => {
 
             }
         }
-
     }
-
-
-
-
-
-
 })
 
-
 function initalise() {
+    currentWord = 0;
+    wordIndex = 0;
     generateWords();
-    document.querySelectorAll('.word')[currentWord].classList.add('current-word');
-    document.querySelectorAll('.letter')[wordIndex].classList.add('current-letter')
-
+    document.querySelectorAll('.word')[0].classList.add('current-word');
+    document.querySelectorAll('.letter')[0].classList.add('current-letter')
 }
 
 function generateWords() {
@@ -58,33 +56,27 @@ function generateWords() {
     wordBox.innerHTML = "";
     for (let i = 0; i < MAX_VISIBLE_WORDS; i++) {
         const word = words[Math.floor(Math.random() * words.length)]
-        // visibleWords.push(word);
         let wordHTML = "";
         for (let i = 0; i < word.length; i++) {
             wordHTML += `<span class="letter">${word[i]}</span>`;
-
         }
         wordBox.innerHTML += `<span class="word">${wordHTML}</span>`;
     }
-
 }
 
 function deleteCharacter(word) {
     if (wordIndex > 0) {
         word.querySelectorAll('.letter')[wordIndex - 1].classList.remove('correct', 'incorrect');
         wordIndex--
-
     } else {
         if (currentWord > 0) {
             currentWord--
             wordIndex = document.querySelectorAll('.word')[currentWord].querySelectorAll('.letter').length - 1
             document.querySelectorAll('.word')[currentWord].querySelectorAll('.letter')[wordIndex].classList.remove('correct', 'incorrect');
-
         }
     }
     document.querySelectorAll('.word')[currentWord].classList.add('current-word');
     document.querySelectorAll('.word')[currentWord].querySelectorAll('.letter')[wordIndex].classList.add('current-letter');
-
 }
 
 function goToNextWord(word) {
